d8824730abe4ce971d5dff010642b47be05d4d19
Aditya Kolsur <kolsur@wisc.edu>
2023-10-25 15:41:52 -0500

    Test learning curve

--- Main_NN.py
+++ Main_NN.py
@@ -5,15 +5,17 @@ from NeuralNetwork import ThreeLayerNN
 
 def main():
     train_loader = torch.utils.data.DataLoader(torchvision.datasets.MNIST('MNIST/', train=True, download=True, transform=torchvision.transforms.Compose([
-                               torchvision.transforms.ToTensor(), torch.flatten])), batch_size=16, shuffle=True)
+                               torchvision.transforms.ToTensor(), torch.flatten])), batch_size=100, shuffle=True)
     test_loader = torch.utils.data.DataLoader(torchvision.datasets.MNIST('MNIST/', train=False, download=True, transform=torchvision.transforms.Compose([
-                               torchvision.transforms.ToTensor(), torch.flatten])), batch_size=1000, shuffle=True)
+                               torchvision.transforms.ToTensor(), torch.flatten])), batch_size=100000, shuffle=True)
 
     epochs = 25
 
     nn_model = ThreeLayerNN()
 
-    for i in range(epochs):
+    learning_curve = {}
+
+    for i in range(1):
         for step, (X_train, y_train) in enumerate(train_loader):
             y_train = y_train.reshape(1, y_train.shape[0])
             y_train = np.eye(10)[y_train].T.reshape(10, y_train.shape[1])
@@ -21,11 +23,19 @@ def main():
             curr_cost = nn_model.train(np.array(X_train), np.array(y_train))
             if step % 100 == 0: print(f'Epoch {i}, Step {step}, Cost = {curr_cost}')
 
+            curr_acc = []
+
+            for idx, (X_test, y_test) in enumerate(test_loader):
+                curr_acc.append(1 - nn_model.test_error(np.array(X_test), np.array(y_test)))
+            learning_curve[(step+1)*100] = np.mean(curr_acc)
+
     accuracy = []
     for idx, (X_test, y_test) in enumerate(test_loader):
         accuracy.append(1 - nn_model.test_error(np.array(X_test), np.array(y_test)))
 
     print(np.mean(accuracy))
+
+    print(learning_curve)
 
 if __name__ == '__main__':
     main()
