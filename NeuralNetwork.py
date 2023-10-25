a67f8dd84ca2aaacc2fa5fe14cf26f5df73442cf
Aditya Kolsur <kolsur@wisc.edu>
2023-10-25 14:36:11 -0500

    Remove bias terms

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -9,11 +9,13 @@ class ThreeLayerNN():
         self.n_h = d1
         self.initialize_weights()
 
-    def initialize_weights(self):
-        self.W1 = np.random.uniform(-1, 1, (self.n_h, self.n_X))
-        #self.b1 = np.zeros((self.n_h, 1))
-        self.W2 = np.random.uniform(-1, 1, (self.n_y, self.n_h))
-        #self.b2 = np.zeros((self.n_y, 1))
+    def initialize_weights(self, method = 'range'):
+        if method == 'zeros':
+            self.W1 = np.zeros((self.n_h, self.n_X))
+            self.W2 = np.zeros((self.n_y, self.n_h))
+        else:
+            self.W1 = np.random.uniform(-1, 1, (self.n_h, self.n_X))
+            self.W2 = np.random.uniform(-1, 1, (self.n_y, self.n_h))
 
     def sigmoid(self, value):
         return 1 / (1 + np.exp(-value))
@@ -26,26 +28,23 @@ class ThreeLayerNN():
         self.y = y_train
         self.m = self.X.shape[0]
         
-        Z1 = np.matmul(self.W1, self.X.T) #+ self.b1
-        A1 = self.sigmoid(Z1)
-        Z2 = np.matmul(self.W2, A1) #+ self.b2
-        A2 = np.exp(Z2) / np.sum(np.exp(Z2), axis=0)
+        z1 = np.matmul(self.W1, self.X.T)
+        a1 = self.sigmoid(z1)
+        z2 = np.matmul(self.W2, a1)
+        f = np.exp(z2) / np.sum(np.exp(z2), axis=0)
 
-        cost = self.loss_func(A2, self.y)
+        cost = self.loss_func(f, self.y)
 
-        dZ2 = A2 - self.y
-        dW2 = (1/self.m) * np.matmul(dZ2, A1.T)
-        # db2 = (1/self.m) * np.sum(dZ2, axis=1, keepdims=True)
-
-        dA1 = np.matmul(self.W2.T, dZ2)
-        dZ1 = dA1 * self.sigmoid(Z1) * (1 - self.sigmoid(Z1))
-        dW1 = (1./self.m) * np.matmul(dZ1, self.X)
-        # db1 = (1./self.m) * np.sum(dZ1, axis=1, keepdims=True)
+        dz2 = f - self.y
+        dW2 = (1/self.m) * np.matmul(dz2, a1.T)
 
         self.W2 = self.W2 - self.lr * dW2
-        # self.b2 = self.b2 - self.lr * db2
+
+        da1 = np.matmul(self.W2.T, dz2)
+        dz1 = da1 * self.sigmoid(z1) * (1 - self.sigmoid(z1))
+        dW1 = (1./self.m) * np.matmul(dz1, self.X)
+
         self.W1 = self.W1 - self.lr * dW1
-        # self.b1 = self.b1 - self.lr * db1
 
         return cost
 
@@ -53,13 +52,10 @@ class ThreeLayerNN():
         return -np.sum(np.multiply(y_label, np.log(y_pred)))
     
     def predict(self, X_test):
-        Z1 = np.matmul(self.W1, X_test.T) #+ self.b1
-        A1 = self.sigmoid(Z1)
-        Z2 = np.matmul(self.W2, A1) #+ self.b2
-        A2 = np.exp(Z2) / np.sum(np.exp(Z2), axis=0)
-
-        predictions = np.argmax(A2, axis=0)
-        return predictions
+        h1 = self.sigmoid(np.matmul(self.W1, X_test.T))
+        z2 = np.matmul(self.W2, h1)
+        g = np.exp(z2) / np.sum(np.exp(z2), axis=0)
+        return np.argmax(g, axis=0)
     
     def test_error(self, X_test, y_test):
         y_pred = self.predict(X_test)
